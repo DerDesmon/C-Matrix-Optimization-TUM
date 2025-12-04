f5379d59b6ebecfacc2b4bdde8241acece12ee47
Simon Gunka <simon.gunka@tum.de>
2025-12-04 17:54:44 +0100

    Chore: remove dev folder entirely

--- Implementierung/dev/benchmark_dev.h
+++
@@ -1,2 +0,0 @@
-#include "../src/matrix_utils.h"
-int benchmark_main(matmul_func matmul);
--- Implementierung/dev/inputs/generators-main.c
+++
@@ -1,9 +0,0 @@
-#include "benchmark-generator.h"
-
-
-int main()
-{
-    generate_matrices_main();
-    //todo: call test generators as well
-    return 0;
-}
